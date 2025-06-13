6b2a7ce90c5ae5611d1568e55a545ddc29e9ed33
aohui.li <aohui.li@witium.com>
2025-06-13 19:00:23 +0800

    ST7588

--- examples/ESP32C3_ESP-IDF/components/Display/Display.c
+++ examples/ESP32C3_ESP-IDF/components/Display/Display.c
@@ -101,7 +101,7 @@ extern u8g2_t u8g2;
 
 void Display_Init(void)
 {
-    u8g2_Setup_ssd1306_i2c_128x64_noname_f(&u8g2, U8G2_R0, u8x8_byte_i2c, u8x8_gpio_and_delay);
+    u8g2_Setup_st7588_i2c_jlx12864_f(&u8g2, U8G2_R0, u8x8_byte_i2c, u8x8_gpio_and_delay);
     u8g2_InitDisplay(&u8g2);                                                                    // 根据所选的芯片进行初始化工作，初始化完成后，显示器处于关闭状态
     u8g2_SetPowerSave(&u8g2, 0);                                                                // 打开显示器
     u8g2_ClearBuffer(&u8g2);
